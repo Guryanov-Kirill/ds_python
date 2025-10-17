86c18add84facb4d75c8ec57a517d285c8b7fc29
Kirill Guryanov <kg580652@gmail.com>
2025-10-17 23:21:41 +0300

    added tests

---
+++ src/test_heap_sort.py
@@ -0,0 +1,30 @@
+import random
+from heap_sort import heap_sort
+
+
+# unit тест
+def test_sort():
+    array = [1, 5, 59, 32, -452, 22, 234, 6, 7]
+    assert heap_sort(array) == [-452, 1, 5, 6, 7, 22, 32, 59, 234]
+
+# критические случаи
+def test_sort():
+    array1 = [1]
+    assert heap_sort(array1) == [1]
+
+    array2 = [5, 4, 3, 2, 1]
+    assert heap_sort(array2) == [1, 2, 3, 4, 5]
+
+    array3 = [2, 5, 67, 3, 5, 7, 54, 67]
+    assert heap_sort(array3) == [2, 3, 5, 5, 7, 54, 67, 67]
+
+
+def test_sort():
+    array4 = [random.randint(-100, 0) for i in range(10)]
+    assert heap_sort(array4) == sorted(array4)
+
+    array5 = [i for i in range(1000, -1000, -1)]
+    assert heap_sort(array5) == sorted(array5)
+
+
+
