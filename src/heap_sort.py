8701b16e1d9b69226f25bbc78a377ed82e0d9d8d
Kirill Guryanov <kg580652@gmail.com>
2025-10-22 11:53:59 +0300

    Bugs fixed

--- src/heap_sort.py
+++ src/heap_sort.py
@@ -1,4 +1,4 @@
-def test(array, i, n):
+def heapify(array, i, n):
     left = 2 * i + 1
     right = 2 * i + 2
     largest = i
@@ -15,14 +15,12 @@ def test(array, i, n):
 def heap_sort(array):
     lenght = len(array)
     for j in range((lenght // 2) - 1, -1, -1):
-        test(array, j, lenght)
+        heapify(array, j, lenght)
     for j in range(lenght - 1, 0, -1):
         array[0], array[j] = array[j], array[0]
-        test(array, 0, j)
+        heapify(array, 0, j)
     return array
 
-array = [1, 5, 59, 32, 452, 22, 234, 6, 7]
-print(heap_sort(array))
 
 
 
--- src/test_heap_sort.py
+++ src/test_heap_sort.py
@@ -2,13 +2,11 @@ import random
 from heap_sort import heap_sort
 
 
-# unit тест
-def test_sort():
+def unit_test():
     array = [1, 5, 59, 32, -452, 22, 234, 6, 7]
     assert heap_sort(array) == [-452, 1, 5, 6, 7, 22, 32, 59, 234]
 
-# критические случаи
-def test_sort():
+def kritical_test():
     array1 = [1]
     assert heap_sort(array1) == [1]
 
@@ -19,7 +17,7 @@ def test_sort():
     assert heap_sort(array3) == [2, 3, 5, 5, 7, 54, 67, 67]
 
 
-def test_sort():
+def property_based_sort():
     array4 = [random.randint(-100, 0) for i in range(10)]
     assert heap_sort(array4) == sorted(array4)
 
